32dddc4fc6f82e99afed1485981c52f0745713fd
champignoom <champignoom@gmail.com>
2020-11-19 14:06:12 +0100

    more skeleton

---
+++ contest.py
@@ -0,0 +1,16 @@
+class Contest:
+    @staticmethod
+    def init_and_open(path):
+        pass
+
+    @staticmethod
+    def open(path):
+        pass
+
+    @staticmethod
+    def close():
+        pass
+
+
+class ContestException(Exception):
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import wx
+from pathlib import Path
+from contest import Contest, ContestException
 
+
+DATA_DIR_NAME = 'data'
+SRC_DIR_NAME = 'src'
+SAMPLE_DIR_NAME = 'sample'
+PROBLEMS_FILE_NAME = 'problems.pdf'
+CONTEST_CONFIG_FILE_NAME = 'config.yaml'
 
 def menu_bar(menus):
     bar = wx.MenuBar()
@@ -22,7 +30,7 @@ def _populate_menu(items):
 
 class MainFrame(wx.Frame):
     TITLE = 'Cena2'
-    SIZE = (350, 250)
+    SIZE = (800, 600)
 
     def menu_item(self, text, handler=None):
         item = wx.MenuItem(id=wx.ID_ANY, text=text);
@@ -36,8 +44,9 @@ class MainFrame(wx.Frame):
         # Menu bar
         self.SetMenuBar(menu_bar([
             ('&File', [
-                self.menu_item('&Open contest'),
-                self.menu_item('&Close contest'),
+                self.menu_item('&Open contest', handler=self._open_contest),
+                self.menu_item('&Close contest', handler=self._close_contest),
+                wx.MenuItem(),
                 self.menu_item('&Properties'),
                 ]),
             ('&Contest', [
@@ -49,6 +58,51 @@ class MainFrame(wx.Frame):
                 self.menu_item('&About'),
                 ]),
             ]))
+
+    def _open_contest_prepare(self):
+        dialog = wx.DirDialog(self, defaultPath=".")
+        dialog.ShowModal()
+        path = dialog.GetPath()
+
+        if not path:
+            return
+
+        path = Path(path)
+        for subdir in (DATA_DIR_NAME, SRC_DIR_NAME):
+            if not (path/subdir).is_dir():
+                wx.MessageBox("Dir '{}' not found in {}".format(subdir, path), style=wx.ICON_ERROR)
+                return
+
+        if not (path/CONTEST_CONFIG_FILE_NAME).exists():
+            yes_no = wx.MessageBox('No config found. Do you want to create a contest here?', style=wx.YES_NO)
+            if yes_no == wx.NO:
+                return
+
+            Contest.init_and_open(path)
+            return
+
+        Contest.open(path)
+
+    def _open_contest(self, ev):
+        try:
+            0 and self._open_contest_prepare()
+        except ContestException:
+            pass
+        else:
+            self._render_contest()
+
+    def _render_contest(self):
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(sizer)
+        self._contest_panel = wx.Panel(self)
+        self._contest_panel.SetBackgroundColour(wx.RED)
+        sizer.Add(self._contest_panel, 1, wx.EXPAND)
+        self.Layout()
+
+    def _close_contest(self, ev):
+        # TODO: disable the menu item after closing contest
+        Contest.close()
+        self._contest_panel.Destroy()
 
 
 def main():
